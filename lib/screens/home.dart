dcbb108d104a660d1a4500bb05b29a3cc1947668
johann ranudd <johann@apidari.no>
2024-03-20 13:44:39 +0100

    basic navigation in place. some issues with on-page navigation due to layout problems. must fix

---
+++ lib/screens/home.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/about');
+          },
+          child: const Text('Go to About'),
+        ),
+      ),
+    );
+  }
+}
